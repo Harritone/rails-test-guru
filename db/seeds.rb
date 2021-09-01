ed60b945d127a767f2c7f0e29f7f9ec76bacb143
Hariton <yourrasik@gmail.com>
2021-09-01 13:45:00 +1000

    Add: seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Answer.destroy_all
+Question.destroy_all
+Quiz.destroy_all
+Category.destroy_all
+
+5.times do
+  category = Category.new(title: Faker::Science.science)
+  category.save!
+  5.times do |i|
+    quiz = Quiz.new(title: Faker::Science.element_subcategory, 
+                    level: i+1, category_id: category.id)
+    quiz.save!
+
+    5.times do |i|
+      question = Question.new(body: Faker::TvShows::SouthPark.quote,
+                              quiz_id: quiz.id)
+      question.save!
+
+      2.times do |i|
+        answer = Answer.new(body: Faker::Science.modifier,
+                            question_id: question.id)
+        answer.save!
+      end
+      answer = Answer.new(body: Faker::Science.modifier,
+                          correct: true, question_id: question.id)
+      answer.save!
+    end
+  end
+end
+
+3.times do
+  user = User.new(email: Faker::Internet.email)
+  user.save!
+end
