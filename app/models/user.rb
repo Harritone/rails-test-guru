655e566ef11e13bf5f3963558d00f1837fbb6507
Hariton <yourrasik@gmail.com>
2021-09-01 18:25:55 +1000

    Add associations and changed #taken_quizzes_by_level

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_many :taken_quizzes
+  has_many :quizzes, through: :taken_quizzes
 
-  def taken_quiz_by_level(level)
-    User.joins(taken_quizzes: :quiz).where(id: id, 'quizzes.level' => level)
+  def taken_quizzes_by_level(level)
+    Quiz.joins(:taken_quizzes).where('taken_quizzes.user_id': id, level: level)
   end
 end
