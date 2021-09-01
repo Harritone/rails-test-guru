09ed4e089760d67fcf03e88c261f7700bf481f4b
Hariton <yourrasik@gmail.com>
2021-09-01 15:04:01 +1000

    Add: Quiz scope titles_by_category

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,4 +1,7 @@
 class Quiz < ApplicationRecord
   belongs_to :category
   has_many :taken_quizzes
+
+  scope :titles_by_cat, ->(category) { joins(:category).where('category.title' => category).pluck(:title).reverse } # => real=0.0017436999987694435
+  scope :titles_by_category, ->(category) { order(title: :desc).joins(:category).where('category.title' => category).pluck(:title) } # real=0.0008589999997639097
 end
