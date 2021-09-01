d7c52e82ddc11896654311ef695ac28674b68c18
Hariton <yourrasik@gmail.com>
2021-09-01 14:28:42 +1000

    Add: adjust seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,43 @@
-User.destroy_all
-Answer.destroy_all
-Question.destroy_all
-Quiz.destroy_all
-Category.destroy_all
+ActiveRecord::Base.transaction do
+  TakenQuiz.destroy_all
+  User.destroy_all
+  Answer.destroy_all
+  Question.destroy_all
+  Quiz.destroy_all
+  Category.destroy_all
 
-5.times do
-  category = Category.new(title: Faker::Science.science)
-  category.save!
-  5.times do |i|
-    quiz = Quiz.new(title: Faker::Science.element_subcategory, 
-                    level: i+1, category_id: category.id)
-    quiz.save!
-
+  5.times do
+    category = Category.new(title: Faker::Science.science)
+    category.save!
     5.times do |i|
-      question = Question.new(body: Faker::TvShows::SouthPark.quote,
-                              quiz_id: quiz.id)
-      question.save!
+      quiz = Quiz.new(title: Faker::Science.element_subcategory, 
+                      level: i+1, category_id: category.id)
+      quiz.save!
 
-      2.times do |i|
+      5.times do |i|
+        question = Question.new(body: Faker::TvShows::SouthPark.quote,
+                                quiz_id: quiz.id)
+        question.save!
+
+        2.times do |i|
+          answer = Answer.new(body: Faker::Science.modifier,
+                              question_id: question.id)
+          answer.save!
+        end
         answer = Answer.new(body: Faker::Science.modifier,
-                            question_id: question.id)
+                            correct: true, question_id: question.id)
         answer.save!
       end
-      answer = Answer.new(body: Faker::Science.modifier,
-                          correct: true, question_id: question.id)
-      answer.save!
     end
   end
-end
 
-3.times do
-  user = User.new(email: Faker::Internet.email)
-  user.save!
+  3.times do
+    user = User.new(email: Faker::Internet.email)
+    user.save!
+  end
+
+  user = User.order(Arel.sql('RANDOM()')).first
+  quiz = Quiz.order(Arel.sql('RANDOM()')).first
+  taken_quiz = TakenQuiz.new(user_id: user.id, quiz_id: quiz.id)
+  taken_quiz.save
 end
