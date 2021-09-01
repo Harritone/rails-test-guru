734cb12ae72cbf86a18ef8bfd1d1c007b4fb07bc
Hariton <yourrasik@gmail.com>
2021-09-01 14:38:18 +1000

    Add: User instance method taken_quiz_by_level

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :taken_quizzes
+
+  def taken_quiz_by_level(level)
+    User.joins(taken_quizzes: :quiz).where(id: id, 'quizzes.level' => level)
+  end
 end
