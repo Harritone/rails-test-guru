a0746130d8e8e5e475485796e7fa1f648c599964
Hariton <yourrasik@gmail.com>
2021-09-07 22:10:22 +1000

    Fix: scope correct

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,10 +4,10 @@ class Answer < ApplicationRecord
   validates :body, presence: true
   validate :number_of_answers_for_question
 
-  scope :all_correct, -> { where(correct: true) }
+  scope :correct, -> { where(correct: true) }
 
   def number_of_answers_for_question
-    cnt = question.answers.count
-    errors.add(:question, "more than 4 answers aren't allowed") if cnt > 4
+    cnt = self.question.answers.count
+    errors.add(:answer, "more than 4 answers aren't allowed") if cnt > 4
   end
 end
