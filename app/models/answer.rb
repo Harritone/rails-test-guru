a619d5a9cd2355a2bbeabe1cee0d0224f464f812
Hariton <yourrasik@gmail.com>
2021-09-07 21:37:37 +1000

    Add: validation of number of answers for question

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,6 +2,12 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
+  validate :number_of_answers_question
 
   scope :all_correct, -> { where(correct: true) }
+
+  def number_of_answers_for_question
+    cnt = question.answers.count
+    errors.add(:question, "more than 4 answers aren't allowed") if cnt > 4
+  end
 end
