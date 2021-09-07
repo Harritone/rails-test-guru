743ff82e114e4402b5c86718c70112e86a03cea6
Hariton <yourrasik@gmail.com>
2021-09-07 14:04:50 +1000

    Fix: error messages

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,7 +6,7 @@ class Question < ApplicationRecord
 
   def number_of_answers
     cnt = answers.count
-    errors.add(:question, 'Should be at least 1 answer') if cnt.zero?
-    errors.add(:question, "More than 4 answers aren't allowed") if cnt > 4
+    errors.add(:question, 'should have at least 1 answer') if cnt.zero?
+    errors.add(:question, "more than 4 answers aren't allowed") if cnt > 4
   end
 end
