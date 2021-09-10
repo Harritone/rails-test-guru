56b9ee7f6b383f5fbc0496a1ec00b1799c65cc07
Hariton <yourrasik@gmail.com>
2021-09-10 13:41:35 +1000

    Add: questions controller

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,72 @@
+class QuestionsController < ApplicationController
+  before_action :set_question, only: %i[show destroy]
+  skip_before_action :verify_authenticity_token, only: %i[create destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :handle_record_not_found
+
+  def index
+    @questions = Question.by_quiz(params[:quiz_id])
+    template = <<~HEREDOC
+      <h1>List of Questions:</h1>
+      <ul>
+        <% @questions.each do |question| %>
+          <li>
+            <form action="/quizzes/#{params[:quiz_id]}/questions/<%= question.id %>" method="POST">
+              <p><%= question.body %></p>
+              <button type="submit">Delete Question</button>
+            </form>
+          </li>
+        <% end %>
+      </ul>
+    HEREDOC
+    respond_to do |format|
+      format.html { render inline: template }
+      format.json { render json: @questions }
+    end
+  end
+
+  def show
+    respond_to do |format|
+      format.html { render inline: '<p><%= @question.body %></p>' }
+      format.json { render json: @question }
+    end
+  end
+
+  def new
+    template = <<~HEREDOC
+      <h1>Create Question</h1>
+      <form action="/quizzes/#{params[:quiz_id]}/questions" method="POST">
+        <label for="question_body">Question Body<label>
+        <textarea name="body" id="question_body" rows="5"></textarea>
+        <input type="hidden" value="#{params[:quiz_id]}" name="quiz_id">
+        <button type="submit">Add Question</button>
+      </form>
+    HEREDOC
+
+    render inline: template
+  end
+
+  def create
+    question = Question.new(question_params)
+    question.save!
+    redirect_to quiz_questions_path
+  end
+
+  def destroy
+    @question.destroy!
+    redirect_to quiz_questions_path
+  end
+
+  private
+
+  def handle_record_not_found
+    render plain: "Sorry, the question you are looking for doesn't exists."
+  end
+
+  def set_question
+    @question = Question.by_quiz(params[:quiz_id]).find(params[:id])
+  end
+
+  def question_params
+    params.permit(:body, :quiz_id)
+  end
+end
