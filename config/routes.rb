6751dead7dd7526723f6a03246e65552bdae2aa6
Hariton <yourrasik@gmail.com>
2021-09-10 13:40:41 +1000

    Add: routes for questions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :quizzes do
+    resources :questions
+  end
+
+  post '/quizzes/:quiz_id/questions/:id', to: 'questions#destroy'
 end
