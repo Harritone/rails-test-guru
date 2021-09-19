a7220a52a1fb3a805012f25acef70053d218c7f2
Hariton <yourrasik@gmail.com>
2021-09-19 19:07:30 +1000

    Add scope of correct answers for quiz

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -6,6 +6,8 @@ class Answer < ApplicationRecord
 
   scope :correct, -> { where(correct: true) }
 
+  scope :correct_for_quiz, ->(quiz_id) { correct.joins(:question).where(questions: { quiz_id: quiz_id })}
+
   def number_of_answers_for_question
     count = question.answers.count
     @errors.add(:answer, "more than 4 answers aren't allowed") if count > 4
