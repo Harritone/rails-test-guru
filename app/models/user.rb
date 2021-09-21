8cc1b5dde0c5089254ef16c82a679406476aefc3
Hariton <yourrasik@gmail.com>
2021-09-21 13:33:03 +1000

    Add: custom auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,31 +1,21 @@
 require 'digest/sha1'
 
 class User < ApplicationRecord
-  # has_many :quiz_passages, class_name: 'Quiz_Passage'
+  include Auth
+
   has_many :taken_quizzes
   has_many :quizzes, through: :taken_quizzes
   has_many :created_quizzes, class_name: 'Quiz',
                              foreign_key: :creator_id,
                              dependent: :nullify
 
-  validates :email, presence: true
   # has_secure_password
 
   def taken_quizzes_by_level(level)
     Quiz.joins(:taken_quizzes).where('taken_quizzes.user_id': id, level: level)
   end
 
-  def authenticate!(password_string)
-    digest(password_string) == self.password_digest ? self : false
-  end
-
   def quiz_passage(quiz)
     taken_quizzes.order(id: :desc).find_by(quiz_id: quiz.id)
-  end
-
-  private
-
-  def digest(string)
-    Digest::SHA1.hexdigest(string)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'quizzes#index'
+  get :sign_up, to: 'users#new'
+  
+  resources :users, only: :create
+
   resources :quizzes do
     resources :questions, shallow: true, except: :index do
       resources :answers, shallow: true, except: :index
