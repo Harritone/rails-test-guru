cca4b35e74177f6eb1310710096001e19f7e94a7
Hariton <yourrasik@gmail.com>
2021-09-29 13:32:57 +1000

    Add: bootstrap classes to views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,12 +7,25 @@ module ApplicationHelper
     link_to repo, "https://github.com/#{author}/#{repo}", target: '_blank'
   end
 
-  def flash_messages
+ def bootstrap_class_for flash_type
+    {
+      success: "alert-success",
+      error: "alert-danger",
+      alert: "alert-warning",
+      notice: "alert-info"
+    }[flash_type.to_sym] || flash_type.to_s
+  end
+
+  def flash_messages(_opts = {})
+    # return if flash[0][0] == :form_error
     flash.each do |msg_type, message|
       next if msg_type == :form_error
-      concat(content_tag(:div, {
-        class: "flash-wrapper"}) do
-        concat content_tag(:p, message.html_safe, class: "flash #{msg_type}")
+      concat(content_tag(:div, message.html_safe, {
+        class: "alert #{bootstrap_class_for(msg_type)} alert-dismissible fade show"}) do
+        concat content_tag(:button, '', class: "btn-close", data: {
+          "bs-dismiss": 'alert'
+          })
+        concat message.html_safe
       end)
     end
     nil
