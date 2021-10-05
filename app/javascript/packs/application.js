3e835ebab98b3d2064f93053714ae8af27c78fff
Hariton <yourrasik@gmail.com>
2021-10-05 11:13:23 +1000

    Add: font-awesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ import * as ActiveStorage from '@rails/activestorage';
 import 'bootstrap/dist/js/bootstrap';
 import 'bootstrap/dist/css/bootstrap';
 import 'channels';
+import '@fortawesome/fontawesome-free/css/all';
 
 Rails.start();
 Turbolinks.start();
