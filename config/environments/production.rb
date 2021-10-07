e3a2916a9ee3857abaef687b0acd30bc7553e10d
Hariton <yourrasik@gmail.com>
2021-10-07 15:05:01 +1000

    try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -183,7 +183,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'localhost', protocol: 'https'}
+  config.action_mailer.default_url_options = { host: 'https://quiz-guru-app.herokuapp.com/gurus/login', protocol: 'https'}
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
